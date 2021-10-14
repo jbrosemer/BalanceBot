bc84a72d4ed3bfe0dc60d256703705cb2c1bae98
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-10-14 19:57:56 -0400

    servo change

--- BalanceBot.py
+++ BalanceBot.py
@@ -13,17 +13,17 @@ imu = MPU9250.MPU9250(bus, address)
 imu.begin()
 try:
     while True:
-        # imu.readSensor()
-        # imu.computeOrientation()
-        kit.servo[15].angle = 180
-        #print ("Accel x: {0} ; Accel y : {1} ; Accel z : {2}".format(imu.AccelVals[0], imu.AccelVals[1], imu.AccelVals[2]))
+        imu.readSensor()
+        imu.computeOrientation()
+        # kit.servo[15].angle = 180
+        # print ("Accel x: {0} ; Accel y : {1} ; Accel z : {2}".format(imu.AccelVals[0], imu.AccelVals[1], imu.AccelVals[2]))
         # print ("Gyro x: {0} ; Gyro y : {1} ; Gyro z : {2}".format(imu.GyroVals[0], imu.GyroVals[1], imu.GyroVals[2]))
         # print ("Mag x: {0} ; Mag y : {1} ; Mag z : {2}".format(imu.MagVals[0], imu.MagVals[1], imu.MagVals[2]))
         # print ("roll: {0} ; pitch : {1} ; yaw : {2}".format(imu.roll, imu.pitch, imu.yaw))
-        # if imu.AccelVals[1] < -5:
-            # kit.continuous_servo[0].throttle = -1
-        # if imu.AccelVals[1] > 5:
-            # kit.continuous_servo[0].throttle = 1
+        if imu.AccelVals[1] < -5:
+            kit.servo[15].angle = 180
+        if imu.AccelVals[1] > 5:
+            kit.servo[15].angle = 0
 except KeyboardInterrupt:
     print()
     print("ended")
