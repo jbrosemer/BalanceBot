467daceeaf23a5c5fff8eeb2e9f1f5b3d3f83dcc
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-10-14 18:18:36 -0400

    mpu python3 test

--- BalanceBot.py
+++ BalanceBot.py
@@ -1,5 +1,6 @@
-from mpu6050 import mpu6050
-mpu = mpu6050(0x68)
+import adafruit_mpu6050
+
+mpu = adafruit_mpu6050(0x68)
 from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 try:
@@ -14,5 +15,6 @@ try:
             kit.continuous_servo[0].throttle = 1
         if gyro_data['y'] < -100:
             kit.continuous_servo[0].throttle = -1
+
 except KeyboardInterrupt:
     print("Stopped")
