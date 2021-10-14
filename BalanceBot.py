f7e72ac68135358ce9bb3e6fd8be604cd8f29ec2
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-10-14 19:40:56 -0400

    trying to add servos again

--- BalanceBot.py
+++ BalanceBot.py
@@ -3,7 +3,8 @@ import os
 import sys
 import time
 import smbus
-
+from adafruit_servokit import ServoKit
+kit = ServoKit(channels=16)
 from imusensor.MPU9250 import MPU9250
 
 address = 0x68
@@ -19,7 +20,10 @@ try:
         # print ("Gyro x: {0} ; Gyro y : {1} ; Gyro z : {2}".format(imu.GyroVals[0], imu.GyroVals[1], imu.GyroVals[2]))
         # print ("Mag x: {0} ; Mag y : {1} ; Mag z : {2}".format(imu.MagVals[0], imu.MagVals[1], imu.MagVals[2]))
         # print ("roll: {0} ; pitch : {1} ; yaw : {2}".format(imu.roll, imu.pitch, imu.yaw))
-        time.sleep(0.1)
+        if imu.AccelVals[1] < -5:
+            kit.continuous_servo[0].throttle = -1
+        if imu.AccelVals[1] > 5:
+            kit.continuous_servo[0].throttle = 1
 except KeyboardInterrupt:
     print()
     print("ended")
