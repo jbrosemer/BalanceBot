2a7dfcbb15f6e149962681112bd582292783199b
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-10-14 19:52:11 -0400

    servo testing

--- BalanceBot.py
+++ BalanceBot.py
@@ -15,7 +15,7 @@ try:
     while True:
         # imu.readSensor()
         # imu.computeOrientation()
-        kit.continuous_servo[15].throttle = -1
+        kit.servo[15].angle = 180
         #print ("Accel x: {0} ; Accel y : {1} ; Accel z : {2}".format(imu.AccelVals[0], imu.AccelVals[1], imu.AccelVals[2]))
         # print ("Gyro x: {0} ; Gyro y : {1} ; Gyro z : {2}".format(imu.GyroVals[0], imu.GyroVals[1], imu.GyroVals[2]))
         # print ("Mag x: {0} ; Mag y : {1} ; Mag z : {2}".format(imu.MagVals[0], imu.MagVals[1], imu.MagVals[2]))
