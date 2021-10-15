fac77d68f423f8bf85bbcca853d8237639ae211a
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-10-14 23:57:13 -0400

    fucked up

--- servotest.py
+++ servotest.py
@@ -4,6 +4,8 @@ kit = ServoKit(channels=16)
 try:
     while True:
         kit.servo[15].angle = 20
+        time.sleep(5)
+        kit.servo[15].angle = 180
 except KeyboardInterrupt:
     print("end")
     kit.servo[15].angle = 2
